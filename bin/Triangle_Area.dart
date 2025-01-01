297d822783ca1b90d8365e472799afd6c5aebb36
Rafiqul Islam <shohag.cse.ku@gmail.com>
2025-01-01 23:07:33 +0600

    updated

---
+++ bin/Triangle_Area.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+void main() {
+  triangle();
+}
+
+void triangle() {
+
+  print("Enter the base of the triangle: ");
+  double base = double.parse(stdin.readLineSync()!);
+
+  print("Enter the height of the triangle: ");
+  double height = double.parse(stdin.readLineSync()!);
+
+
+  double area = (base * height) / 2;
+
+
+  print('The area of the triangle is: $area');
+}
